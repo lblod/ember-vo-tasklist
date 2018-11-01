617320725c23dad2e41865517506772729e61a2f
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-11-01 17:04:19 +0100

    improve performance
    - create only task-solution when state changes
    - all task-solutions belong to flat list in tasklist-solution

--- addon/components/task-solution/edit.js
+++ addon/components/task-solution/edit.js
@@ -3,27 +3,54 @@ import layout from '../../templates/components/task-solution/edit';
 import { computed } from '@ember/object';
 import { task } from 'ember-concurrency';
 import mapAsync from '../../utils/map-async';
+import findAsync from '../../utils/find-async';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   layout,
   classNames: ['task-solution'],
+  store: service(),
+  taskStatus: false,
 
   setChilds: task(function *(){
-    let taskSolChilds = yield this.taskSolution.taskSolutionChilds;
-    //load tasks
-    yield mapAsync(taskSolChilds, (c) => c.task);
-    this.set('sortedTaskSolutionChilds', taskSolChilds.toArray().sort((a,b) => a.get('task.priority') - b.get('task.priority')));
+    let childs = (yield this.task.taskChilds).toArray();
+    childs = childs.sort((a,b)=> a.priority - b.priority);
+    this.set('sortedChilds', childs);
   }),
 
-  async didReceiveAttrs(){
+  setStatus: task( function *(){
+    let taskSolution = yield findAsync((yield this.tasklistSolution.taskSolutions).toArray(),
+                                       async sol => (await sol.task).get('id') == this.task.id);
+
+    if(taskSolution){
+      this.set('taskStatus', taskSolution.status);
+    }
+  }),
+
+  didReceiveAttrs(){
     this._super(...arguments);
-    this.setChilds.perform();
+    if(this.task){
+      this.setStatus.perform();
+      this.setChilds.perform();
+    }
+  },
+
+  async updateTaskSolutions(task, status){
+    let taskSolution = await findAsync((await this.tasklistSolution.taskSolutions).toArray(),
+                                       async sol => (await sol.task).get('id') == task.id);
+
+    if(!taskSolution){
+      taskSolution = this.store.createRecord('task-solution', { task, status });
+      this.tasklistSolution.taskSolutions.pushObject(taskSolution);
+      return;
+    }
+
+    taskSolution.set('status', status);
   },
 
   actions:{
     async toggleStatus(){
-      this.taskSolution.set('status', !this.taskSolution.status);
-      await this.taskSolution.save();
+      await this.updateTaskSolutions(this.task, !this.taskStatus);
     }
   }
 });
--- addon/components/tasklist-solution/edit.js
+++ addon/components/tasklist-solution/edit.js
@@ -11,40 +11,11 @@ export default Component.extend({
   classNames: ['tasklist-solution'],
   store: service(),
 
-  async generateTaskSolutionTree(taskSolutions, task, taskSolutionParent = null){
-    let taskSolution = await this.findOrCreateTaskSolution(task, taskSolutions);
-    if(taskSolutionParent){
-      taskSolution.set('taskSolutionParent', taskSolutionParent);
-      await taskSolution.save();
-    }
-
-    let taskChilds = await task.taskChilds;
-    await mapAsync(taskChilds, async child => this.generateTaskSolutionTree(taskSolutions, child, taskSolution));
-    return taskSolutionParent || taskSolution;
-  },
-
-  async findOrCreateTaskSolution(task, taskSolutions){
-    let taskSolution = await findAsync(taskSolutions, async sol => (await sol.task).get('id') == task.id);
-
-    if(!taskSolution){
-        taskSolution = this.store.createRecord('task-solution', { task });
-        await taskSolution.save();
-    }
-    return taskSolution;
-  },
-
   setTaskSolutions: task(function *(){
     let tasklist = yield this.tasklistSolution.tasklist;
-    let topLevelTasks = yield tasklist.tasks; //TODO: it seems we cannot fetch nested stuff async??
-    let taskSolutions = yield flattenAsync((yield this.tasklistSolution.get('taskSolutions')).toArray() || [],
-                                                async node => (await node.get('taskSolutionChilds')).toArray());
-
+    let topLevelTasks = yield tasklist.tasks;
     topLevelTasks = topLevelTasks.toArray().sort((a,b)=> a.priority - b.priority);
-    let updatedTaskSolutions = yield mapAsync(topLevelTasks, async task => this.generateTaskSolutionTree(taskSolutions, task));
-
-    this.tasklistSolution.taskSolutions.setObjects(updatedTaskSolutions);
-    yield this.tasklistSolution.save();
-    this.set('sortedTaskSolutions', this.tasklistSolution.taskSolutions);
+    this.set('tasks', topLevelTasks);
   }),
 
   didReceiveAttrs() {
